9c984f9c05a382e4696bdb67c7bab1a942783264
EthanColbert8 <emcolbert2@gmail.com>
2026-02-26 11:08:42 -0500

    include configs for different dump policies, switch default to torch backend

--- start_server.sh
+++ start_server.sh
@@ -4,17 +4,21 @@
 MODEL_FOLDER="/depot/cms/users/colberte/SONIC/sonic-inference-replay/single_sonic_model/models"
 DUMP_FOLDER="/depot/cms/users/colberte/SONIC/sonic-inference-replay/replay_dumps"
 
-TRITON_CONTAINER="/depot/cms/users/colberte/SONIC/triton_25.07.sif"
-MY_PACKAGES="/depot/cms/private/users/colberte/conda_envs/torchonnxCUDA/lib/python3.12/site-packages"
-CONTAINER_PACKAGES="/usr/local/lib/python3.12/dist-packages"
+TRITON_CONTAINER="/depot/cms/users/colberte/SONIC/triton_python_cuda.sif"
+# TRITON_CONTAINER="/depot/cms/users/colberte/SONIC/triton_25.07.sif"
+# MY_PACKAGES="/depot/cms/private/users/colberte/conda_envs/torchonnxCUDA/lib/python3.12/site-packages"
+# CONTAINER_PACKAGES="/usr/local/lib/python3.12/dist-packages"
 
-apptainer run --nv -B "${MODEL_FOLDER}:/models" -B "${DUMP_FOLDER}:/dumps" \
-    -B "${MY_PACKAGES}/functorch:${CONTAINER_PACKAGES}/functorch" \
-    -B "${MY_PACKAGES}/torch:${CONTAINER_PACKAGES}/torch" \
-    -B "${MY_PACKAGES}/torch-2.6.0+cu126.dist-info:${CONTAINER_PACKAGES}/torch-2.6.0+cu126.dist-info" \
-    -B "${MY_PACKAGES}/torchgen:${CONTAINER_PACKAGES}/torchgen" \
-    $TRITON_CONTAINER tritonserver --model-repository=/models
+# apptainer run --nv -B "${MODEL_FOLDER}:/models" -B "${DUMP_FOLDER}:/dumps" \
+#     -B "${MY_PACKAGES}/functorch:${CONTAINER_PACKAGES}/functorch" \
+#     -B "${MY_PACKAGES}/torch:${CONTAINER_PACKAGES}/torch" \
+#     -B "${MY_PACKAGES}/torch-2.6.0+cu126.dist-info:${CONTAINER_PACKAGES}/torch-2.6.0+cu126.dist-info" \
+#     -B "${MY_PACKAGES}/torchgen:${CONTAINER_PACKAGES}/torchgen" \
+#     $TRITON_CONTAINER tritonserver --model-repository=/models
     # --trace-config triton,file=/dumps/trace2.json \
     # --trace-config triton,log-frequency=1 \
     # --trace-config rate=1 \
     # --trace-config level=TENSORS
+
+apptainer run --nv -B "${MODEL_FOLDER}:/models" -B "${DUMP_FOLDER}:/dumps" \
+    $TRITON_CONTAINER tritonserver --model-repository=/models --model-config-name=dump_always
